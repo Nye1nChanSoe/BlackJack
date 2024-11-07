bed366a42d24d7e5d60d8a73ed8dff48797c6f05
Nyein Chan Soe <nyeinchansoe.business@gmail.com>
2024-11-07 21:19:16 +0700

    Add shuffling animation

--- src/game.py
+++ src/game.py
@@ -6,10 +6,9 @@ from typing import List
 import pygame
 import random
 
-
+# TODO: separate rendering logic to renderer class (optional)
 # TODO: card flip animation at the game end
 # TODO: card draw animation for player and dealer
-# TODO: deck shuffle animation (optional)
 class Game:
     def __init__(self, screen: pygame.Surface, font: pygame.font.Font, debug: bool):
         self.screen = screen
@@ -31,16 +30,20 @@ class Game:
         self.hit_button = Button((340, 550), (80, 36), "Hit", self.font)
         self.stand_button = Button((440, 550), (80, 36), "Stand", self.font)
 
-        self.random_no_of_cards = random.randint(10, 14)
-        self.random_offsets_x = [random.randint(-5, 5) for _ in range(self.random_no_of_cards)]
-        self.random_offsets_y = [random.randint(-5, 5) for _ in range(self.random_no_of_cards)]
-        self.random_rotates = [random.randint(-12, 12) for _ in range(self.random_no_of_cards)]
-
         self.init_deck()
         self.init_player()
         self.init_dealer()
 
         self.check_initial_blackjack()
+
+        self.is_shuffled = False
+        self.shuffle_frames = 10
+        self.shuffle_frame_counter = 0
+        self.random_no_of_cards = 10
+        self.random_offsets_x = [0] * self.random_no_of_cards
+        self.random_offsets_y = [0] * self.random_no_of_cards
+        self.random_rotates = [0] * self.random_no_of_cards
+
 
     def init_deck(self):
         self.deck.shuffle()
@@ -71,16 +74,18 @@ class Game:
                 self.update_dealer(event)
 
     def render(self):
-        self.render_player_hands()
-        self.render_deck_stack()
-        self.render_game_result()
+        if not self.is_shuffled:
+            self.render_shuffle()
+        else:
+            self.render_deck_stack()
+            self.render_player_hands()
+            self.render_dealer_hands()
+            if self.player_turn and not self.game_over:
+                self.hit_button.draw(self.screen)
+                self.stand_button.draw(self.screen)
+        if self.game_over:
+            self.render_game_result()
         self.render_score()
-        self.render_dealer_hands()
-
-        if self.player_turn and not self.game_over:
-            self.hit_button.draw(self.screen)
-            self.stand_button.draw(self.screen)
-
 
     def update_player(self, event: pygame.event.Event):
         if self.hit_button.is_clicked(event) and len(self.player.hand) < 5:
@@ -131,7 +136,7 @@ class Game:
         card_offset = 60
         x, y = 100, 60
 
-        dealer_caption = self.font.render("Dealer", True, (255, 255, 255))            
+        dealer_caption = self.font.render("Dealer", True, (255, 255, 255))
 
         for i, card in enumerate(self.dealer.hand):
             if self.game_over and card.is_hidden:
@@ -145,38 +150,57 @@ class Game:
         self.screen.blit(dealer_caption, (x, y - 40))
         self.screen.blit(text_surface, (x + 10, y + 150))
 
+    def render_shuffle(self):
+        x, y = 660, 140
+        image = pygame.image.load('assets/cards/cardback.png').convert_alpha()
+        if self.shuffle_frame_counter < self.shuffle_frames:
+            for i in range(self.random_no_of_cards):
+                self.random_offsets_x[i] = random.randint(-30, 30)
+                self.random_offsets_y[i] = random.randint(-10, 30)
+                self.random_rotates[i] = random.randint(-20, 20)
+                rotated_image = pygame.transform.rotozoom(image, self.random_rotates[i], 1)
+                rotated_rect = rotated_image.get_rect(center=(x + self.random_offsets_x[i], y + self.random_offsets_y[i] + i * 2))
+                self.screen.blit(rotated_image, rotated_rect.topleft)
+            deck_caption = self.font.render(f"Shuffling...", True, (255, 255, 255))
+            self.screen.blit(deck_caption, (x - 40, y - 114))
+            self.shuffle_frame_counter += 1
+        else:
+            self.is_shuffled = True
+
     def render_deck_stack(self):
         x, y = 660, 140
         image = pygame.image.load('assets/cards/cardback.png').convert_alpha()
         deck_caption = self.font.render(f"Deck: {len(self.deck)}", True, (255, 255, 255))
+
+        # Use the final shuffled positions and rotations
         for i in range(self.random_no_of_cards):
             random_offset_x = self.random_offsets_x[i]
             random_offset_y = self.random_offsets_y[i]
-            random_rotations = self.random_rotates[i]
-            rotated_image = pygame.transform.rotozoom(image, random_rotations, 1)
+            random_rotation = self.random_rotates[i]
+            rotated_image = pygame.transform.rotozoom(image, random_rotation, 1)
             rotated_rect = rotated_image.get_rect(center=(x + random_offset_x, y + random_offset_y + i * 2))
             self.screen.blit(rotated_image, rotated_rect.topleft)
+
         self.screen.blit(deck_caption, (x - 40, y - 114))
 
     def render_game_result(self):
-        if self.game_over:
-            if self.player_won:
-                result_color = (0, 255, 0)
-            elif "Tie" in self.game_result:
-                result_color = (255, 255, 0)
-            else:
-                result_color = (255, 0, 0)
+        if self.player_won:
+            result_color = (0, 255, 0)
+        elif "Tie" in self.game_result:
+            result_color = (255, 255, 0)
+        else:
+            result_color = (255, 0, 0)
 
-            result_surface = self.font.render(self.game_result, True, result_color)
-            text_width = result_surface.get_width()
-            screen_center_x = self.screen.get_width() // 2
-            centered_x = screen_center_x - (text_width // 2)
-            self.screen.blit(result_surface, (centered_x, self.screen.get_height() // 2 - 20))
+        result_surface = self.font.render(self.game_result, True, result_color)
+        text_width = result_surface.get_width()
+        screen_center_x = self.screen.get_width() // 2
+        centered_x = screen_center_x - (text_width // 2)
+        self.screen.blit(result_surface, (centered_x, self.screen.get_height() // 2 - 20))
 
-            # Display message to restart
-            restart_message = self.font.render("Press 'R' to restart", True, (255, 255, 255))
-            restart_message_width = restart_message.get_width()
-            self.screen.blit(restart_message, (screen_center_x - (restart_message_width // 2), self.screen.get_height() // 2 + 20))
+        # Display message to restart
+        restart_message = self.font.render("Press 'R' to restart", True, (255, 255, 255))
+        restart_message_width = restart_message.get_width()
+        self.screen.blit(restart_message, (screen_center_x - (restart_message_width // 2), self.screen.get_height() // 2 + 20))
 
     def render_score(self):
         # Render the win and lose tally in the bottom-right corner
@@ -225,12 +249,17 @@ class Game:
         self.dealer_turn = False
         self.game_over = False
         self.player_won = False
-        self.random_no_of_cards = random.randint(10, 14)
-        self.random_offsets_x = [random.randint(-5, 5) for _ in range(self.random_no_of_cards)]
-        self.random_offsets_y = [random.randint(-5, 5) for _ in range(self.random_no_of_cards)]
-        self.random_rotates = [random.randint(-12, 12) for _ in range(self.random_no_of_cards)]
         self.game_result = ''
         self.deck.reset()
         self.init_deck()
         self.init_player()
         self.init_dealer()
+
+        self.is_shuffled = False
+        self.shuffle_frames = 10
+        self.shuffle_frame_counter = 0
+        self.random_no_of_cards = 10
+        self.random_offsets_x = [0] * self.random_no_of_cards
+        self.random_offsets_y = [0] * self.random_no_of_cards
+        self.random_rotates = [0] * self.random_no_of_cards
+
